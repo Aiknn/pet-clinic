9a3f7dc66395060377021f77d064266cd9346a89
Yerlan Nurlibayev <ynurlibayev@outlook.com>
2022-09-04 00:42:58 +0600

    feat: Implement Map Based Services. Closes #6

---
+++ src/main/java/kz/aiknn/petclinic/services/map/VetServiceMap.java
@@ -0,0 +1,34 @@
+package kz.aiknn.petclinic.services.map;
+
+import kz.aiknn.petclinic.model.Vet;
+import kz.aiknn.petclinic.services.CrudService;
+
+import java.util.Set;
+
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Vet object) {
+        super.delete(object);
+    }
+
+}
