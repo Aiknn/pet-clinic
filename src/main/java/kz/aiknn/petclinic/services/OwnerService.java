de848526fe182371a063c9018372dfbd8e4b25aa
Yerlan Nurlibayev <ynurlibayev@outlook.com>
2022-09-04 00:04:54 +0600

    refactor: Add common crud interface. Closes #1

--- src/main/java/kz/aiknn/petclinic/services/OwnerService.java
+++ src/main/java/kz/aiknn/petclinic/services/OwnerService.java
@@ -2,13 +2,7 @@ package kz.aiknn.petclinic.services;
 
 import kz.aiknn.petclinic.model.Owner;
 
-import java.util.Set;
-
-public interface OwnerService {
+public interface OwnerService extends CrudService<Owner, Long>{
 
     Owner findByLastName(String lastName);
-    Owner findById(Long id);
-    Owner save(Owner owner);
-    Set<Owner> findAll();
-
 }
